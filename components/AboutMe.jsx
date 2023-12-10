8089e03e784e2ba723defe79c5fc894dcec5b116
Ng Kah Jun <kahjun_ng@yahoo.com>
2023-12-10 19:41:21 +0800

    first commit

---
+++ components/AboutMe.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+import { useTransition, useState } from "react";
+import TabButton from "./TabButton";
+
+//1d
+const TAB_DATA = [
+    {
+        title: "Skills",
+        id: "skills",
+        content: (
+          <ul className="list-disc pl-2">
+                <li>c#</li>
+                <li>c++</li>
+                <li>Java</li>
+                <li>Python</li>
+          </ul>
+        ),
+    },
+    {
+        title: "Experiences",
+        id: "experiences",
+        content: (
+          <ul className="list-disc pl-2">
+            <li>None</li>
+          </ul>
+        ),
+    },
+    {
+        title: "Education",
+        id: "education",
+        content: (
+          <ul className="list-disc pl-2">
+                <li>Asia Pacific University</li>
+                <li>SMK Seksyen 4 Bandar Kinrara</li>
+          </ul>
+        ),
+    }
+];
+//1
+const AboutMe = () => {
+    //1b
+    const[tab, setTab] = useState("skills");
+    const[isPending, startTransition] = useTransition();
+    const handleTabChange = (id) => {
+        startTransition(() => {
+            setTab(id);
+        });
+    };
+    //1a
+    return(
+        <section className="text-white grid" id="aboutMe">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:px-32 py-16 xl-gap-16 items-center">
+                <div>
+                    <img src="/AboutMe.png" alt="About Me" width={450} height={450} style={{ borderRadius: "25px" }}/>
+                </div>
+                <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+                    <h1 className="text-4xl font-bold text-white mb-4">About Me</h1>
+                    <p className="text-base text-justify lg:text-lg">Hi , my name is Ng Kah Jun and I am 20 years old. Currently, I am still pursuing my degree in cyber security at Asia Pacific University. 
+I have learned programming languages such as Python, C Programming and Java. I have also learned additional domain specific programming language such as MSSQL and R programming. Additionally, I have acquired skills to operate on Operating System such as Kali Linux and Ubuntu. 
+Besides that, I am a good follower as I will  go over my limits and finish the tasks given to me. I am also a curious person as I am not afraid to ask questions to improve my skills and myself as a person. (Write own 3rd soft skill) . 
+I like to play badminton every week as i believe it is a very good stress reliever after a long and hectic week</p>
+                    <div className="flex flex-row mt-8">
+{/* 1e */}
+                        <TabButton 
+                        selectTab={() => handleTabChange("skills")}
+                        active= {tab === "skills"}
+                        >
+                            
+                            Skills
+                        </TabButton>
+
+                        <TabButton 
+                        selectTab={() => handleTabChange("experiences")}
+                        active= {tab === "experiences"}
+                        >
+                            {" "}
+                            Experiences{" "}
+                        </TabButton>
+
+                        <TabButton 
+                        selectTab={() => handleTabChange("education")}
+                        active= {tab === "education"}
+                        >
+                            {" "}
+                            Education{" "}
+                        </TabButton>
+                        
+                    </div>
+                    <div className="mt-8">{TAB_DATA.find((t) => t.id === tab).content}</div>
+                </div>
+
+            </div>
+        </section>
+
+    );
+};
+export default AboutMe;
